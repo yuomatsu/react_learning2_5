28137e01e21e4c2dd8257f134a3498b160668a1e
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-01-01 21:20:47 +0900

    19. 基本的なページ遷移

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import { InlineStyle } from "./components/InlineStyle";
-import { CssModules } from "./components/CssModules";
-import { StyledJsx } from "./components/StyledJsx";
-import { StyledComponents } from "./components/StyledComponents";
-import { Emotion } from "./components/Emotion";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <InlineStyle />
-      <CssModules />
-      <StyledJsx />
-      <StyledComponents />
-      <Emotion />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
---
+++ src/Page2.jsx
@@ -0,0 +1,7 @@
+export const Page2 = () => {
+  return (
+    <div>
+      <h1>Page2</h1>
+    </div>
+  );
+};
