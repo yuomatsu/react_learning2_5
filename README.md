3266350309e63c6d436e1a1afce475c1aadf2daa
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-01-01 20:51:42 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# react_learning2_5
+# react_learning1_2
 Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
+import { Emotion } from "./components/Emotion";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
+    </div>
+  );
+}
