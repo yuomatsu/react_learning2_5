9fd2e04247f38e77953b32adc0ffaea167d699c4
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-01-02 18:51:22 +0900

    20. ネストされたページ遷移

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,7 @@
+export const Page1DetailA = () => {
+  return (
+    <div>
+      <h1>Page1 DetailAページです</h1>
+    </div>
+  );
+};
---
+++ src/Page1DetailB.jsx
@@ -0,0 +1,7 @@
+export const Page1DetailB = () => {
+  return (
+    <div>
+      <h1>Page1 DetailBページです</h1>
+    </div>
+  );
+};
