64cd298684c63ee4b75aebea34c4c665dbfabcbd
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-01-02 23:01:38 +0900

    23. クエリパラメーター

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
 export const Page2 = () => {
   return (
     <div>
       <h1>Page2</h1>
+      <Link to="/page2/999">URL Parameterてすと</Link>
     </div>
   );
 };
