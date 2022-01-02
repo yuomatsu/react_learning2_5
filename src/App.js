a0ef4e691bbed9f14fa3c08aeb5c268d5494e5d3
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-01-02 19:23:22 +0900

    21. ルーティング分割

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
-import { Page2 } from "./Page2";
+import { Router } from "./router/Router";
 
 import "./styles.css";
 
@@ -18,30 +13,7 @@ export default function App() {
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/page1"
-          render={({ match: { url } }) => (
-            <Switch>
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route exact path={`${url}/detailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route exact path={`${url}/detailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
