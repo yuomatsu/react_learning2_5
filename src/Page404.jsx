446de77d49b935ffd7c29a85aa45f057593bee6c
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-01-03 19:47:50 +0900

    26. 404ページ

---
+++ src/Page404.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export const Page404 = () => {
+  return (
+    <div>
+      <h1>404ページです</h1>
+      <Link to="/">トップに戻る</Link>
+    </div>
+  );
+};
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
 import { page1Routes } from "./Page1Routes";
 import { page2Routes } from "./Page2Routes";
+import { Page404 } from "../Page404";
 
 export const Router = () => {
   return (
@@ -41,6 +42,9 @@ export const Router = () => {
           </Switch>
         )}
       />
+      <Route path="*">
+        <Page404 />
+      </Route>
     </Switch>
   );
 };
